cf55e6114af227c6156e7ba99fee371302026e3d
Your Name <you@example.com>
2017-04-04 23:08:40 +0800

    完成慕课网django入门和实践的URL详细配置

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url
-
+from django.conf.urls import  url
 from . import views
 
 urlpatterns = [
-    
-    url(r'^index/$', views.index),
+    # Examples:
+    # url(r'^$', 'myblog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^index/$',views.index ),
+    url(r'^article/(?P<article_id>[0-9]+)$',views.article_page,name="article_page")
+   
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from  . import models
+# from django.http import HttpResponse
+
 # Create your views here.
+
 def index(request):
-    return render(request,'index.html')
+	# return HttpResponse('HELLO WORLD')
+	articles = models.Article.objects.all()
+	return render(request,'index.html',{'articles':articles})
+	# return render(request,'index.html',{'hello':'hello today!!!'})
+
+def article_page(request,article_id):
+	article = models.Article.objects.get(pk=article_id)
+	return render(request,'article_page.html',{'article':article})
+
