3c499e78f337f0010ad70275d0907fd94813909f
Your Name <you@example.com>
2017-04-06 06:42:52 +0800

    完成博客页面模板路由，修改文章有bug,文章对象无法传递

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # url(r'^$', 'myblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^index/$',views.index ),
-    url(r'^article/(?P<article_id>[0-9]+)$',views.article_page,name="article_page")
-   
+    url(r'^article/(?P<article_id>[0-9]+)$',views.article_page,name="article_page"),
+   url(r'^edit/(?P<article_id>[0-9]+)$',views.edit_page,name='edit_page'),
+   url(r'^edit/actions$',views.edit_action,name='edit_action'),
+
 ]
